b080d01d7175112e9ad1a406828d32b82ec9a647
Shiju Palamthodi <shiju.palamthodi@valtech.com>
2023-03-02 15:42:06 +0000

    Initial service setup with health endpoint test

---
+++ src/main/kotlin/uk/gov/dluhc/emsintegrationapi/EmsIntegrationApiApplication.kt
@@ -0,0 +1,18 @@
+package uk.gov.dluhc.emsintegrationapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+/**
+ * Spring Boot application bootstrapping class.
+ */
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
+class EmsIntegrationApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmsIntegrationApiApplication>(*args)
+}
