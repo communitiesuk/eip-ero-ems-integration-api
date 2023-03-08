813ef0d08dc065f43ddd0518e190a368f89b3b41
Shiju Palamthodi <shiju.palamthodi@valtech.com>
2023-03-08 09:11:22 +0000

    Revinfo table script removed

--- src/main/resources/db/changelog/create/0001_revinfo.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
--- changeset Krister.Bone@valtech.com:create-revinfo-table endDelimiter:;
-
-CREATE TABLE `revinfo`
-(
-    `rev`      int NOT NULL AUTO_INCREMENT,
-    `revtstmp` bigint ,
-    PRIMARY KEY (`rev`)
-)
