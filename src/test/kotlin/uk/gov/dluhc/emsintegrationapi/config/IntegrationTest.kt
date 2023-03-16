e27423fa6ac3491082d1333a34e8e9627930fc8a
Shiju Palamthodi <shiju.palamthodi@valtech.com>
2023-03-16 12:09:20 +0000

    Removing unnecessary variable

--- src/test/kotlin/uk/gov/dluhc/emsintegrationapi/config/IntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/emsintegrationapi/config/IntegrationTest.kt
@@ -1,20 +1,17 @@
 package uk.gov.dluhc.emsintegrationapi.config
 
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.web.reactive.server.WebTestClient
 
 private val logger = KotlinLogging.logger {}
+
 /**
  * Base class used to bring up the entire Spring ApplicationContext
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration-test")
 @AutoConfigureWebTestClient(timeout = "PT5M")
-abstract class IntegrationTest {
-    @Autowired
-    protected lateinit var webTestClient: WebTestClient
-}
+abstract class IntegrationTest
+
