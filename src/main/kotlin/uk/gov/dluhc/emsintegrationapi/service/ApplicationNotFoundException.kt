57a09e9fbe00830afd5d20395c25e4da41eecf5d
Shiju Palamthodi <shiju.palamthodi@valtech.com>
2023-04-13 16:31:37 +0100

    Ktlint fix

--- src/main/kotlin/uk/gov/dluhc/emsintegrationapi/service/ApplicationNotFoundException.kt
+++ src/main/kotlin/uk/gov/dluhc/emsintegrationapi/service/ApplicationNotFoundException.kt
@@ -4,4 +4,3 @@ import uk.gov.dluhc.emsintegrationapi.exception.ResourceNotFoundException
 
 class ApplicationNotFoundException(val applicationId: String, val applicationType: ApplicationType) :
     ResourceNotFoundException("The ${applicationType.displayName} application could not be found with id `$applicationId`")
-
