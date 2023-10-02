05c4b2050a0d5ab6e0098c80da283b0835526a82
Tomos Slater <Tomos.Slater@softwire.com>
2023-10-02 10:46:29 +0100

    [EIP1-6984] Fix ktlint errors

--- src/main/kotlin/uk/gov/dluhc/emsintegrationapi/mapper/ProxyVoteMapper.kt
+++ src/main/kotlin/uk/gov/dluhc/emsintegrationapi/mapper/ProxyVoteMapper.kt
@@ -3,7 +3,6 @@ package uk.gov.dluhc.emsintegrationapi.mapper
 import org.apache.commons.codec.binary.Base64
 import org.springframework.stereotype.Component
 import uk.gov.dluhc.emsintegrationapi.database.entity.ProxyVoteApplication
-import uk.gov.dluhc.emsintegrationapi.database.entity.Type as TypeEntity
 import uk.gov.dluhc.emsintegrationapi.models.Address
 import uk.gov.dluhc.emsintegrationapi.models.ApplicationLanguage
 import uk.gov.dluhc.emsintegrationapi.models.ApplicationStatus
@@ -14,7 +13,7 @@ import uk.gov.dluhc.emsintegrationapi.models.RejectedReasonItem
 import uk.gov.dluhc.emsintegrationapi.models.RejectedReasons
 import uk.gov.dluhc.emsintegrationapi.database.entity.Address as AddressEntity
 import uk.gov.dluhc.emsintegrationapi.database.entity.RejectedReasonItem as RejectedReasonItemEntity
-
+import uk.gov.dluhc.emsintegrationapi.database.entity.Type as TypeEntity
 
 @Component
 class ProxyVoteMapper(private val instantMapper: InstantMapper) {
@@ -99,9 +98,9 @@ class ProxyVoteMapper(private val instantMapper: InstantMapper) {
         }
     }
 
-    fun mapRejectedReasonItemFromEntity(rejectedReasonItem: RejectedReasonItemEntity) : RejectedReasonItem {
+    fun mapRejectedReasonItemFromEntity(rejectedReasonItem: RejectedReasonItemEntity): RejectedReasonItem {
         return with(rejectedReasonItem) {
-            RejectedReasonItem (
+            RejectedReasonItem(
                 electorReason = electorReason,
                 type = when (type) {
                     TypeEntity.IDENTITY_NOT_CONFIRMED -> RejectedReasonItem.Type.IDENTITY_MINUS_NOT_MINUS_CONFIRMED
@@ -114,7 +113,6 @@ class ProxyVoteMapper(private val instantMapper: InstantMapper) {
                     TypeEntity.PROXY_LIMITS -> RejectedReasonItem.Type.PROXY_MINUS_LIMITS
                     TypeEntity.PROXY_NOT_REGISTERED_TO_VOTE -> RejectedReasonItem.Type.PROXY_MINUS_NOT_MINUS_REGISTERED_MINUS_TO_MINUS_VOTE
                     TypeEntity.OTHER_REJECT_REASON -> RejectedReasonItem.Type.OTHER_MINUS_REJECT_MINUS_REASON
-
                 },
                 includeInComms = includeInComms
             )
