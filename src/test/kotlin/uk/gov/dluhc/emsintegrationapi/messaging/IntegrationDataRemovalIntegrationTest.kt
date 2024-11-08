cf0f41e1ef053de490f92965a8897761373c262e
Simon Payne <Simon.Payne@softwire.com>
2024-11-08 09:48:48 +0000

    EIP1-11125: remove error response definition from openapi data removal message specification; make several imports explicit not wildcard

--- src/test/kotlin/uk/gov/dluhc/emsintegrationapi/messaging/IntegrationDataRemovalIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/emsintegrationapi/messaging/IntegrationDataRemovalIntegrationTest.kt
@@ -16,15 +16,18 @@ import uk.gov.dluhc.emsintegrationapi.config.MESSAGE_ID
 import uk.gov.dluhc.emsintegrationapi.cucumber.common.StepHelper.Companion.deleteRecords
 import uk.gov.dluhc.emsintegrationapi.cucumber.common.StepHelper.Companion.deleteSqsMessage
 import uk.gov.dluhc.emsintegrationapi.cucumber.common.StepHelper.TestPhase
+import uk.gov.dluhc.emsintegrationapi.database.entity.ApplicationDetails
 import uk.gov.dluhc.emsintegrationapi.database.repository.PostalVoteApplicationRepository
 import uk.gov.dluhc.emsintegrationapi.database.repository.ProxyVoteApplicationRepository
 import uk.gov.dluhc.emsintegrationapi.messaging.models.RemoveVoterApplicationEmsDataMessage
 import uk.gov.dluhc.emsintegrationapi.messaging.models.RemoveVoterApplicationEmsDataMessage.Source.POSTAL
+import uk.gov.dluhc.emsintegrationapi.messaging.models.RemoveVoterApplicationEmsDataMessage.Source.PROXY
 import uk.gov.dluhc.emsintegrationapi.testsupport.TestLogAppender
 import uk.gov.dluhc.emsintegrationapi.testsupport.TestLogAppender.Companion.logs
 import uk.gov.dluhc.emsintegrationapi.testsupport.assertj.assertions.ILoggingEventAssert.Companion.assertThat
-import uk.gov.dluhc.emsintegrationapi.testsupport.testdata.getRandomString
-import java.util.UUID.randomUUID
+import uk.gov.dluhc.emsintegrationapi.testsupport.testdata.buildPostalVoteApplication
+import uk.gov.dluhc.emsintegrationapi.testsupport.testdata.buildProxyVoteApplication
+import uk.gov.dluhc.emsintegrationapi.testsupport.testdata.randomHexadecimalString
 import java.util.concurrent.TimeUnit
 
 class IntegrationDataRemovalIntegrationTest : IntegrationTest() {
@@ -68,10 +71,15 @@ class IntegrationDataRemovalIntegrationTest : IntegrationTest() {
 
         @Test
         fun `should log removal of postal ems integration data`() {
-            val applicationId = getRandomString(24)
+            val applicationId = randomHexadecimalString(24)
+            val postalVoteApplication = buildPostalVoteApplication(applicationId = applicationId)
+            postalVoteApplication.applicationDetails.emsStatus = ApplicationDetails.EmsStatus.SUCCESS
+
+            postalVoteApplicationRepository.save(postalVoteApplication)
+
             val payload = RemoveVoterApplicationEmsDataMessage(
                 applicationId,
-                RemoveVoterApplicationEmsDataMessage.Source.POSTAL,
+                POSTAL,
             )
 
             sqsMessagingTemplate.convertAndSend(
@@ -97,6 +105,45 @@ class IntegrationDataRemovalIntegrationTest : IntegrationTest() {
                     assertThat(it).`as`("Message: ${it.message}")
                         .hasMessageId(messageId)
                 }
+            }
+        }
+    }
+
+    @Test
+    fun `should log removal of proxy ems integration data`() {
+        val applicationId = randomHexadecimalString(24)
+        val proxyVoteApplication = buildProxyVoteApplication(applicationId = applicationId)
+        proxyVoteApplication.applicationDetails.emsStatus = ApplicationDetails.EmsStatus.SUCCESS
+
+        proxyVoteApplicationRepository.save(proxyVoteApplication)
+
+        val payload = RemoveVoterApplicationEmsDataMessage(
+            applicationId,
+            PROXY,
+        )
+
+        sqsMessagingTemplate.convertAndSend(
+            removeApplicationEmsDataQueueName,
+            payload
+        )
+
+        await.atMost(10, TimeUnit.SECONDS).untilAsserted {
+            assertThat(proxyVoteApplicationRepository.findById(payload.applicationId)).isNotNull
+
+            val firstReliableMessage = TestLogAppender.getLogEventMatchingRegex(
+                "^Deleting $PROXY application ems data with id = .{24}$",
+                Level.INFO
+            )
+            assertThat(firstReliableMessage).hasAnyMessageId()
+            val messageId = firstReliableMessage!!.mdcPropertyMap[MESSAGE_ID]!!
+
+            val filteredLogs = logs.filter { it.mdcPropertyMap.isNotEmpty() }
+            // wait until sufficient messages are logged before verifying all share same correlation ID and message ID
+            assertThat(filteredLogs).hasSizeGreaterThanOrEqualTo(3)
+
+            filteredLogs.forEach {
+                assertThat(it).`as`("Message: ${it.message}")
+                    .hasMessageId(messageId)
             }
         }
     }
