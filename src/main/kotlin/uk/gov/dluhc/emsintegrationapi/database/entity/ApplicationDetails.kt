638343daa27a8f55f9f9451bf1c1e0ab6a9da043
SimPaySW <Simon.Payne@softwire.com>
2025-03-27 16:02:07 +0000

    EIP1-12292: annotate ems_status with explicit type for use when saving application received from SQS (#128)
    * EIP1-12292: add new EmsStatus enum constant named INIT; when saving application, make mapper assign this new value to newly created records; add integration test assertions to check this is being assigned correctly for postal & proxy
    
    * EIP1-12292: change @Enumerated tag on emsStatus field to ORDINAL from STRING to align with existing data
    
    * EIP1-12292: remove enumerated tag from emsStatus field
    
    * EIP1-12292: add JdbcType annotation to emsSttaus field to help hibernate work out how to handle it; also migrate ems_status column from varchar to int
    
    * EIP1-12292: add comment
    
    * Update src/main/kotlin/uk/gov/dluhc/emsintegrationapi/database/entity/ApplicationDetails.kt
    
    Co-authored-by: Ralf Leggett <112947118+ralfleggettsoftwire@users.noreply.github.com>
    
    * EIP1-12292: correct internal naming of migration XML; put all rollback statements in one block
    
    * EIP1-12292: correct rollback of ems_status datatype change to revert to previous varchar(20) not int
    
    ---------
    
    Co-authored-by: Ralf Leggett <112947118+ralfleggettsoftwire@users.noreply.github.com>

--- src/main/kotlin/uk/gov/dluhc/emsintegrationapi/database/entity/ApplicationDetails.kt
+++ src/main/kotlin/uk/gov/dluhc/emsintegrationapi/database/entity/ApplicationDetails.kt
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
 import jakarta.validation.constraints.Size
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 import java.time.Instant
 
 @Embeddable
@@ -31,7 +33,9 @@ class ApplicationDetails(
     @field:Size(max = 500)
     val signatureWaivedReason: String?,
 
-    @Enumerated(EnumType.STRING)
+    // Hibernate 6 gets confused unless the column type is stated explicitly
+    @JdbcTypeCode(SqlTypes.INTEGER)
+    @Enumerated(EnumType.ORDINAL)
     var emsStatus: EmsStatus?,
 
     var emsMessage: String?,
@@ -45,6 +49,7 @@ class ApplicationDetails(
         REJECTED
     }
 
+    // NB this is persisted as ORDINAL so new types must be added to the end of the list of constants
     enum class EmsStatus {
         SUCCESS,
         FAILURE
