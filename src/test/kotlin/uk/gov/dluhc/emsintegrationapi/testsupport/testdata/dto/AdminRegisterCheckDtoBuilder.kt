e1fc5ca7a4b11ebe0d93dd69327504db4f45536a
DanWilkinsSoftwire <dan.wilkins@softwire.com>
2025-11-05 17:24:59 +0000

    EROPSPT-538: Add application reference to admin api output

--- src/test/kotlin/uk/gov/dluhc/emsintegrationapi/testsupport/testdata/dto/AdminRegisterCheckDtoBuilder.kt
+++ src/test/kotlin/uk/gov/dluhc/emsintegrationapi/testsupport/testdata/dto/AdminRegisterCheckDtoBuilder.kt
@@ -6,11 +6,13 @@ import java.util.UUID
 
 fun buildAdminPendingRegisterCheckDto(
     sourceReference: String = UUID.randomUUID().toString(),
+    applicationReference: String? = "V2RJ8HPQA3",
     gssCode: String = "E09000021",
     createdAt: Instant? = null,
     historicalSearch: Boolean? = null,
 ) = AdminPendingRegisterCheckDto(
     sourceReference = sourceReference,
+    applicationReference = applicationReference,
     gssCode = gssCode,
     createdAt = createdAt,
     historicalSearch = historicalSearch,
