4d4efa9755188249cfb4c7b81b1d3ff6f6ecfbaf
Connor Whitworth <connor.whitworth@softwire.com>
2026-08-26 11:32:34 +0100

    EROPSPT-548: Use messagingsupport.MessageListener for SQS (#234)

--- src/main/kotlin/uk/gov/dluhc/emsintegrationapi/messaging/ProcessIntegrationDataRemovalMessageListener.kt
+++ src/main/kotlin/uk/gov/dluhc/emsintegrationapi/messaging/ProcessIntegrationDataRemovalMessageListener.kt
@@ -7,16 +7,19 @@ import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
 import uk.gov.dluhc.emsintegrationapi.messaging.models.RemoveApplicationEmsIntegrationDataMessage
 import uk.gov.dluhc.emsintegrationapi.service.ProcessIntegrationDataRemovalMessageService
+import uk.gov.dluhc.messagingsupport.MessageListener
 
 private val logger = KotlinLogging.logger { }
 
 @Component
-class ProcessIntegrationDataRemovalMessageListener(private val removeEmsIntegrationDataMessageService: ProcessIntegrationDataRemovalMessageService) {
+class ProcessIntegrationDataRemovalMessageListener(
+    private val removeEmsIntegrationDataMessageService: ProcessIntegrationDataRemovalMessageService
+) : MessageListener<RemoveApplicationEmsIntegrationDataMessage> {
 
     @SqsListener("\${sqs.remove-application-ems-integration-data-queue-name}")
-    fun handleMessage(@Valid @Payload removeEmsDataMessage: RemoveApplicationEmsIntegrationDataMessage) {
-        with(removeEmsDataMessage) {
-            logger.info { "Integration Data Removal Message received with an application id = ${removeEmsDataMessage.applicationId} and source ${removeEmsDataMessage.source}" }
+    override fun handleMessage(@Valid @Payload payload: RemoveApplicationEmsIntegrationDataMessage) {
+        with(payload) {
+            logger.info { "Integration Data Removal Message received with an application id = $applicationId and source $source" }
             removeEmsIntegrationDataMessageService.process(this)
         }
     }
